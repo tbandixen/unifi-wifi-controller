2e8b96384688d9dadab017b234e39a68db688ada
Thomas Bandixen <thomas@bandixen.ch>
2020-03-16 13:27:30 +0100

    script quotes changed

--- nightly/endisable.sh
+++ nightly/endisable.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-curl -s --header "Content-Type: application/json" --request POST --data '$1' https://wifistateservice.azurewebsites.net/state
+curl -s --header "Content-Type: application/json" --request POST --data "$1" https://wifistateservice.azurewebsites.net/state
