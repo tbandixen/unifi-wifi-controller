4c78794f1eb5feb1d9778c31ad84f8b4996d3cbe
Thomas Bandixen <thomas@bandixen.ch>
2020-03-30 16:48:36 +0200

    updated with env variables

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 REM -v %cd%/.ssh:/root/.ssh:rw
 REM -v %cd%/1min:/etc/periodic/1min:rw
-docker run --rm -i -t -e SSH_USER=tbx -e API_URL=https://wifistateservice.azurewebsites.net/state --name unifi-wifi-controller unifi-wifi-controller
+docker run --rm -i -t -e SSH_USER=tbx -e API_URL=https://wifistateservice.azurewebsites.net/state -e INTERFACES=2;1,unifi-ap.bx.home,wifi0;2,unifi-ap-ac-lite.bx.home,wifi0,wifi1 --name unifi-wifi-controller unifi-wifi-controller:latest
 IF ERRORLEVEL 1 PAUSE
